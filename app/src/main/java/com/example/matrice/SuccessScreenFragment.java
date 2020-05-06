fc088525b374fc4acbf5510bf61783f52ca6b2b6
Csertan <csertant@gmail.com>
2020-05-06 15:23:14 +0200

    Adds SuccessScreenFragment and updates navigation
    Signed-off-by: Csertan <csertant@gmail.com>

--- app/src/main/java/com/example/matrice/SuccessScreenFragment.java
+++ app/src/main/java/com/example/matrice/SuccessScreenFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 
 /**
@@ -17,6 +20,17 @@ public class SuccessScreenFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Setup any handles to view objects here
+        ImageButton toHomeButton = (ImageButton) view.findViewById(R.id.homeButtonSuccess);
+        toHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(MainNavGraphDirections.actionPopUpToMainScreenFragment());
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
