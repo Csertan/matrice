f6cc7a14888afc4e41b61426ebfef92374d62318
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2020-08-19 12:14:32 +0200

    Settings Screen fixes [2/3]

--- app/src/main/java/com/nosebite/matrice/SettingsActivity.java
+++ app/src/main/java/com/nosebite/matrice/SettingsActivity.java
@@ -2,7 +2,6 @@ package com.nosebite.matrice;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -62,27 +61,33 @@ public class SettingsActivity extends BaseActivity {
             user = FirebaseAuth.getInstance().getCurrentUser();
             parentActivity = (SettingsActivity) getActivity();
 
+            /* Setting correct version name */
+            String versionName = BuildConfig.VERSION_NAME;
+            Preference versionPreference = findPreference(getString(R.string.key_version_name));
+            assert versionPreference != null;
+            versionPreference.setSummary(versionName);
+
             /* Summary provider for Figure Set Preferences */
             ListPreference figureSetPreferences = findPreference(getString(R.string.key_figure_set));
-            if(figureSetPreferences != null) {
+            if (figureSetPreferences != null) {
                 figureSetPreferences.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
             }
 
             /* Summary provider for Transition Type Preferences */
             ListPreference transitionTypePreferences = findPreference(getString(R.string.key_transition_type));
-            if(transitionTypePreferences != null) {
+            if (transitionTypePreferences != null) {
                 transitionTypePreferences.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
             }
 
             /* Summary provider for Language Preferences */
             ListPreference languagePreferences = findPreference(getString(R.string.key_app_language));
-            if(languagePreferences != null) {
+            if (languagePreferences != null) {
                 languagePreferences.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
             }
 
             /* Summary provider for Gender Preferences */
             ListPreference genderPreferences = findPreference(getString(R.string.key_gender_info));
-            if(genderPreferences != null) {
+            if (genderPreferences != null) {
                 genderPreferences.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
             }
             /* Update Database with changed gender preferences */
@@ -94,7 +99,7 @@ public class SettingsActivity extends BaseActivity {
 
             /* Update Database with age preferences on change */
             EditTextPreference agePreferences = findPreference(getString(R.string.key_age_info));
-            if(agePreferences != null) {
+            if (agePreferences != null) {
                 agePreferences.setSummaryProvider(EditTextPreference.SimpleSummaryProvider.getInstance());
             }
             assert agePreferences != null;
@@ -102,7 +107,7 @@ public class SettingsActivity extends BaseActivity {
                 String pattern = "^[0-9]+$";
                 Pattern regexp = Pattern.compile(pattern);
                 Matcher matcher = regexp.matcher(newValue.toString());
-                if(matcher.find() && Integer.parseInt(newValue.toString()) < 125) {
+                if (matcher.find() && Integer.parseInt(newValue.toString()) < 125) {
                     users.child(user.getUid()).child("age").setValue(newValue);
                     return true;
                 }
@@ -122,10 +127,9 @@ public class SettingsActivity extends BaseActivity {
             SwitchPreference darkModePreference = findPreference(getString(R.string.key_enable_dark_mode));
             assert darkModePreference != null;
             darkModePreference.setOnPreferenceChangeListener((preference, newValue) -> {
-                if(!darkModePreference.isChecked()) {
+                if (!darkModePreference.isChecked()) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                else {
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
                 return true;
@@ -141,7 +145,7 @@ public class SettingsActivity extends BaseActivity {
 
             /* On CLick Listener for viewing Privacy Policy on website */
             Preference privacyPolicy = findPreference(getString(R.string.key_privacy_policy));
-            if(privacyPolicy != null) {
+            if (privacyPolicy != null) {
                 privacyPolicy.setOnPreferenceClickListener(preference -> {
                     openWebPage(getString(R.string.url_privacy));
                     return true;
@@ -151,17 +155,16 @@ public class SettingsActivity extends BaseActivity {
 
         /**
          * Opens the desired webpage in the default browser.
+         *
          * @param url String type URL of the webpage
          */
         void openWebPage(String url) {
             Uri webPage = Uri.parse(url);
             Intent webIntent = new Intent(Intent.ACTION_VIEW);
             webIntent.setData(webPage);
-            if(webIntent.resolveActivity(requireActivity().getPackageManager()) != null)
-            {
+            if (webIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
                 startActivity(webIntent);
-            }
-            else {
+            } else {
                 Toast.makeText(getContext(), getString(R.string.error_view_webpage), Toast.LENGTH_LONG).show();
             }
         }
@@ -169,19 +172,15 @@ public class SettingsActivity extends BaseActivity {
 
     /**
      * Sends Feedback with device specifications to the developer.
+     *
      * @param context Actual context from the function is called.
      */
     public static void sendFeedback(@NonNull Context context) {
-        String body = null;
-        try {
-            body = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
-            body = "\n\n--------------------\nPlease don't remove the " +
-                    "information below!\n--------------------\n Device OS: Android \n Device OS version: " +
-                    Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
-                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        String body = BuildConfig.VERSION_NAME;
+        body = "\n\n--------------------\nPlease don't remove the " +
+                "information below!\n--------------------\n Device OS: Android \n Device OS version: " +
+                Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
+                "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"nosebitestudios@gmail.com"});
